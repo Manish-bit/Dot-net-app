0f3749624137ca037a3c37cc713a5d037991adfe
Manish-Bit <manisharyal517@gmail.com>
2022-06-19 20:20:55 +0545

    Feature: about page added

---
+++ Project/Controllers/LawyerController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project.Controllers
+{
+    public class LawyerController : Controller
+    {
+        public IActionResult Lawyer()
+        {
+            return View();
+        }
+        public IActionResult LawyerSingle()
+        {
+            return View();
+        }
+    }
+}
